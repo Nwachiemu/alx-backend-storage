77f497aab2622ebe8343de8877f14c0692edac1b
Nwachiemu <emuchaynwachi@gmail.com>
2024-06-16 12:42:14 +0100

    added

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,38 +1,31 @@
 #!/usr/bin/env python3
-"""
-Python script that analyzes Nginx logs stored in MongoDB and prints statistics.
-"""
+'''Task 12's module.
+'''
+from pymongo import MongoClient
 
-import pymongo
 
-def print_collection_stats(collection):
-    # Total number of logs
-    total_logs = collection.count_documents({})
-
-    print(f"{total_logs} logs")
-
-    # Methods count
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print("Methods:")
+def print_nginx_request_logs(nginx_collection):
+    '''Prints stats about Nginx request logs.
+    '''
+    print('{} logs'.format(nginx_collection.count_documents({})))
+    print('Methods:')
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
-        count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+        req_count = len(list(nginx_collection.find({'method': method})))
+        print('\tmethod {}: {}'.format(method, req_count))
+    status_checks_count = len(list(
+        nginx_collection.find({'method': 'GET', 'path': '/status'})
+    ))
+    print('{} status check'.format(status_checks_count))
 
-    # Count for method=GET and path=/status
-    status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status_check_count} status check")
 
-def main():
-    # Connect to MongoDB
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
-    db = client.logs
-    collection = db.nginx
+def run():
+    '''Provides some stats about Nginx logs stored in MongoDB.
+    '''
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    print_nginx_request_logs(client.logs.nginx)
 
-    # Print statistics
-    print_collection_stats(collection)
 
-    # Close MongoDB connection
-    client.close()
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    run()
+    
